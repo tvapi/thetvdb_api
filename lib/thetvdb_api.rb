5059a0e1be8bed8a35e936f68680b5366f9d78ea
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2013-12-01 16:30:38 +0100

    episode model

--- lib/thetvdb_api.rb
+++ lib/thetvdb_api.rb
@@ -1,6 +1,7 @@
 require 'thetvdb_api/version'
 require 'thetvdb_api/configuration'
 require 'thetvdb_api/utility'
+require 'thetvdb_api/episode'
 require 'thetvdb_api/series'
 
 module ThetvdbApi; end
