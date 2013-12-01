ba6f5330a56c43992a5858f1f5d88afef8fd929d
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2013-12-01 15:48:30 +0100

    new test for date coerce

--- spec/thetvdb_api/utility/date_spec.rb
+++ spec/thetvdb_api/utility/date_spec.rb
@@ -5,5 +5,9 @@ describe Date do
     it 'should convert string to date' do
       Date.coerce('2001-02-03').should == Date.new(2001, 2, 3)
     end
+
+    it 'should return nil for no date string' do
+      Date.coerce('test').should == nil
+    end
   end
 end
